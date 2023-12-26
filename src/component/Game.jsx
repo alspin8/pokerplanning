95f282c7cd6a476c9cf6fc1347c616e95717a5fe
Loris_Dematini <loris.dematini@yahoo.fr>
2023-12-26 15:53:30 +0100

    Système de tours
    Ajout du système de tours (pas fini), et du système du jeu

--- src/component/Game.jsx
+++ src/component/Game.jsx
@@ -1,17 +1,17 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import {ReactComponent as Tapis     } from "../resource/svg/tapis.svg";
-import {ReactComponent as Card0     } from "../resource/svg/0.svg";
-import {ReactComponent as Card1     } from "../resource/svg/1.svg";
-import {ReactComponent as Card1_2   } from "../resource/svg/1_2.svg";
-import {ReactComponent as Card2     } from "../resource/svg/2.svg";
-import {ReactComponent as Card3     } from "../resource/svg/3.svg";
-import {ReactComponent as Card5     } from "../resource/svg/5.svg";
-import {ReactComponent as Card8     } from "../resource/svg/8.svg";
-import {ReactComponent as Card13    } from "../resource/svg/13.svg";
-import {ReactComponent as Card20    } from "../resource/svg/20.svg";
-import {ReactComponent as Card40    } from "../resource/svg/40.svg";
-import {ReactComponent as Card100   } from "../resource/svg/100.svg";
+import { ReactComponent as Tapis } from "../resource/svg/tapis.svg";
+import { ReactComponent as Card0 } from "../resource/svg/0.svg";
+import { ReactComponent as Card1 } from "../resource/svg/1.svg";
+import { ReactComponent as Card1_2 } from "../resource/svg/1_2.svg";
+import { ReactComponent as Card2 } from "../resource/svg/2.svg";
+import { ReactComponent as Card3 } from "../resource/svg/3.svg";
+import { ReactComponent as Card5 } from "../resource/svg/5.svg";
+import { ReactComponent as Card8 } from "../resource/svg/8.svg";
+import { ReactComponent as Card13 } from "../resource/svg/13.svg";
+import { ReactComponent as Card20 } from "../resource/svg/20.svg";
+import { ReactComponent as Card40 } from "../resource/svg/40.svg";
+import { ReactComponent as Card100 } from "../resource/svg/100.svg";
 // import {ReactComponent as CardInter } from "../resource/svg/inter.svg";
 import usePlayer from "../hooks/UsePlayer";
 import useTask from "../hooks/UseTask";
@@ -33,52 +33,88 @@ const Game = () => {
     const modeHook = useState(config.modes[0]);
     const [mode, setMode] = modeHook;
 
+    const [currentTaskIndex, setCurrentTaskIndex] = useState(0);
+    const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
+    const [cardPlayed, setCarPlayed] = useState(0);
+    const [playersPlayed, setPlayersPlayed] = useState(0);
+
     const gameStateHook = useState("config");
     const [gameState, setGameState] = gameStateHook;
 
     console.log(tasks);
     console.log(players);
+    console.log(config.cards);
+
+    const endTurn = () => {
+        setPlayersPlayed(0);
+        setCurrentPlayerIndex((prevIndex) => (prevIndex + 1) % players.length);
+        if (cardPlayed !== ""){
+            playCard();
+        }
+        else{
+            alert("Vous devez sélectionnez une carte avant de poursuivre.");
+        }
+        
+    };
+
+    const updateCard = () => {
+        setCarPlayed()
+
+    }
+
+    const playCard = () => {
+        setPlayersPlayed((prevCount) => prevCount + 1);
+
+        if (playersPlayed === players.length - 1) {
+            setPlayersPlayed(0);
+            setCurrentTaskIndex((prevIndex) => (prevIndex + 1) % tasks.length);
+        }
+    };
 
     return (
         <>
-          {gameState === "config" && (
-            <>
-              <LandingPage
-                config={config}
-                modeHook={modeHook}
-                tasksHook={tasksHook}
-                playersHook={playersHook}
-                gStateHook={gameStateHook}
-              />
-            </>
-          )}
-    
-          {gameState !== "config" && (
-            <>
-              <Tapis style={{ width: "70%", height: "auto" }} />
-              
-              {/* Test d'affichage des taches et joueurs pour voir si le passage à une autre page marche. */}
-              <div>
-                <h2>Tâches choisies :</h2>
-                <ul>
-                    
-                  {tasks.map((task, index) => (
-                    <li key={index}>{task.text}</li>
-                  ))}
-                </ul>
-              </div>
-              <div>
-                <h2>Joueurs choisis :</h2>
-                <ul>
-                  {players.map((player, index) => (
-                    <li key={index}>{player}</li>
-                  ))}
-                </ul>
-              </div>
-            </>
-          )}
+            {gameState === "config" && (
+                <>
+                    <LandingPage
+                        config={config}
+                        modeHook={modeHook}
+                        tasksHook={tasksHook}
+                        playersHook={playersHook}
+                        gStateHook={gameStateHook}
+                    />
+                </>
+            )}
+
+            {gameState !== "config" && (
+                <>
+                    <Tapis style={{ width: "40%", height: "auto", marginLeft: "30%" }} />
+
+                    <div style={{ textAlign: "center" }}>
+                        <h3>{tasks[currentTaskIndex]?.text}</h3>
+                        <h2>{`${players[currentPlayerIndex]}, à toi de jouer`}</h2>
+
+                        <div style={{ display: "flex", justifyContent: "center" }}>
+                            {config.cards.map((Card, index) => (
+                                <div key={index} style={{ margin: "5px" }}>
+                                    <Card
+                                        style={{ width: "50px", height: "auto" }}
+                                        onClick={updateCard}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+
+                        <button
+                            style={{ display: "block", margin: "20px auto", width: "60px" }}
+                            onClick={endTurn}
+                        >
+                            Fin du tour
+                        </button>
+                    </div>
+                </>
+            )}
         </>
-      );
-    };
+    );
+};
 
 export default Game;
