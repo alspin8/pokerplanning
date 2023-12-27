2677603e97cf37b58470cc1cd7569c42f814a490
Loris_Dematini <loris.dematini@yahoo.fr>
2023-12-27 17:00:45 +0100

    Mise en forme
    Remise en forme du document

--- src/component/Game.jsx
+++ src/component/Game.jsx
@@ -13,7 +13,8 @@ import { ReactComponent as Card13 } from "../resource/svg/13.svg";
 import { ReactComponent as Card20 } from "../resource/svg/20.svg";
 import { ReactComponent as Card40 } from "../resource/svg/40.svg";
 import { ReactComponent as Card100 } from "../resource/svg/100.svg";
-// import {ReactComponent as CardInter } from "../resource/svg/inter.svg";
+import { ReactComponent as CardCafe } from "../resource/svg/PauseCafe.svg";
+
 import usePlayer from "../hooks/UsePlayer";
 import useTask from "../hooks/UseTask";
 import GameSetting from "./GameSetting";
@@ -23,21 +24,20 @@ const Game = () => {
 
     const config = {
         modes: ["mean", "majority"],
-        cards: [CardInter, Card0, Card1, Card1_2, Card2, Card3, Card5, Card8, Card13, Card20, Card40, Card100],
+        cards: [CardInter, Card0, Card1, Card1_2, Card2, Card3, Card5, Card8, Card13, Card20, Card40, Card100, CardCafe],
         maxPlayer: 4
     }
 
     const playersHook = usePlayer();
-    const [players, addPlayer, removePlayer, setPlayerCardTurn] = playersHook;
+    const [players, addPlayer, removePlayer, setPlayer] = playersHook;
     const tasksHook = useTask();
-    const [tasks, addTask, removeTask, setTaskText, setTaskCard] = tasksHook;
+    const [tasks, addTask, removeTask, setTask] = tasksHook;
     const modeHook = useState(config.modes[0]);
     const [mode, setMode] = modeHook;
 
     const [currentTaskIndex, setCurrentTaskIndex] = useState(0);
     const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
     const [cardPlayed, setCardPlayed] = useState(0);
-    const [playerCard, setPlayerCard] = useState(0);
     const [playersPlayed, setPlayersPlayed] = useState(0);
 
     const gameStateHook = useState("config");
@@ -48,11 +48,9 @@ const Game = () => {
 
     //Fin du tour (clique sur le bouton)
     const endTurn = () => {
-        //console.log(playerCard);
-        //MARCHE PAS
-        console.log(currentPlayerIndex);
-        console.log(cardPlayed);
-        setPlayerCardTurn(currentPlayerIndex, cardPlayed);
+        // console.log(currentPlayerIndex);
+        // console.log(cardPlayed);
+        setPlayer(currentPlayerIndex, { card: cardPlayed });
 
         setCurrentPlayerIndex((prevIndex) => (prevIndex + 1) % players.length);
         playCard();
@@ -61,10 +59,8 @@ const Game = () => {
     //Choix de la carte joué 
     const updateCard = (cardIndex) => {
         setCardPlayed(cardIndex);
-    //     setPlayerCard(() => {
-    //         return cardIndex;});
     }
-    
+
     // Fonction pour passer au tour suivant si une carte a été choisi
     const playCard = () => {
         setCardPlayed(0);
@@ -95,7 +91,7 @@ const Game = () => {
 
                     <div className="titre_text">
                         <h3>{tasks[currentTaskIndex]?.text}</h3>
-                        <h2>{`${players[currentPlayerIndex]}, à toi de jouer`}</h2>
+                        <h2>{`${players[currentPlayerIndex].text}, à toi de jouer`}</h2>
 
                         <div className="card_list">
                             {config.cards.map((Card, index) => (
@@ -115,7 +111,7 @@ const Game = () => {
                             Fin du tour
                         </button>
                     </div>
-                    </>
+                </>
             )}
         </>
     );
